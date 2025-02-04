c0732cbad3235582fc916a63d12f8c6221c72c69
odanroterif <143523077+odanroterif@users.noreply.github.com>
2025-02-04 09:21:02 -0300

    Add files via upload

---
+++ gitusable/components/boxes.tsx
@@ -0,0 +1,7 @@
+import { Alert } from "react-native";
+
+
+
+export default function beta(){
+    Alert.alert("AVISO", "Esta função virá em uma atualização futura")
+}
---
+++ gitusable/components/imports.tsx
@@ -0,0 +1,4 @@
+export {default as box } from '../components/boxes';
+export {default as styles} from "../components/style";
+export {default as Tester} from "../components/tester";
+export {default as er} from "../screens/error";
