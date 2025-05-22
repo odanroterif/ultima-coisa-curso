61fea7c3ebfec9feeae9f7f8d4fd0d2a1c4e9d91
odanroterif <143523077+odanroterif@users.noreply.github.com>
2025-05-22 14:22:12 -0300

    Update script.js

--- qrcode/script/script.js
+++ qrcode/script/script.js
@@ -11,13 +11,15 @@ function QRgenerate()
 {
     qrcode.innerHTML = "";
     
-    const qr = new QRCode(qrcode, {
-        text: url.value,
-        width: 300,
-        height: 300,
-        colorDark : "gray",
-        colorLight : "#ffffff",        
-    });    
-    qrcode.style.display = "block";
+    if(url.value !== ""){
+        const qr = new QRCode(qrcode, {
+            text: url.value,
+            width: 300,
+            height: 300,
+            colorDark : "#7e7e7e",
+            colorLight : "rgba(0,0,0,0)",                    
+        });           
+    }
+
     url.value = "";
 }
