1e6a83f3586488c3669b0d0ce5b750c7e94b0cd0
odanroterif <143523077+odanroterif@users.noreply.github.com>
2025-05-28 16:02:18 -0300

    Update script.js

--- qrcode/script/script.js
+++ qrcode/script/script.js
@@ -4,21 +4,34 @@ const qrcode = document.querySelector('#qr');
 
 
 
-urlButton.addEventListener("click",QRgenerate);
+urlButton.addEventListener("click", QRgenerate);
+url.addEventListener("keydown", (event) => { if (event.key === 'Enter') { QRgenerate() } });
 
 
-function QRgenerate()
-{
+function QRgenerate() {
     qrcode.innerHTML = "";
-    
-    if(url.value !== ""){
-        const qr = new QRCode(qrcode, {
-            text: url.value,
-            width: 300,
-            height: 300,
-            colorDark : "#7e7e7e",
-            colorLight : "rgba(0,0,0,0)",                    
-        });           
+
+    const identifier = '[a-zA-Z0-9]'
+    let indent = 0
+
+    if (url.value !== "") {
+        for (var c = 0; c < url.value.length; c++) {            
+
+            url.value[c].match(identifier) === false ? indent = 0 : indent++;
+
+            if (indent === 0 && c === url.length - 1) {
+                const qr = new QRCode(qrcode, {
+                    text: url.value,
+                    width: 300,
+                    height: 300,
+                    colorDark: "#7e7e7e",
+                    colorLight: "rgba(0,0,0,0)",
+                });
+            } else {
+                alert("Please enter a valid URL");
+                break;
+            }
+        }
     }
 
     url.value = "";
