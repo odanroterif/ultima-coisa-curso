3026e3761bfdfa8271a1c32cbcb9f86a138eb610
odanroterif <143523077+odanroterif@users.noreply.github.com>
2025-06-01 14:30:40 -0300

    Update script.js

--- qrcode/script/script.js
+++ qrcode/script/script.js
@@ -3,36 +3,27 @@ const urlButton = document.querySelector("#btn");
 const qrcode = document.querySelector('#qr');
 
 
-
 urlButton.addEventListener("click", QRgenerate);
 url.addEventListener("keydown", (event) => { if (event.key === 'Enter') { QRgenerate() } });
-
 
 function QRgenerate() {
     qrcode.innerHTML = "";
 
-    const identifier = '[a-zA-Z0-9]'
-    let indent = 0
+    var verify = 0;
 
-    if (url.value !== "") {
-        for (var c = 0; c < url.value.length; c++) {            
-
-            url.value[c].match(identifier) === false ? indent = 0 : indent++;
-
-            if (indent === 0 && c === url.length - 1) {
-                const qr = new QRCode(qrcode, {
-                    text: url.value,
-                    width: 300,
-                    height: 300,
-                    colorDark: "#7e7e7e",
-                    colorLight: "rgba(0,0,0,0)",
-                });
-            } else {
-                alert("Please enter a valid URL");
-                break;
-            }
-        }
+    if (/[À-ž]/.test(url.value)) {
+        verify = 1;
+        alert("FRANCISCO NÃO ACEITA CARACTER ESPECIAL");
     }
 
+    if (url.value !== "" && verify === 0) {
+        const qr = new QRCode(qrcode, {
+            text: url.value,
+            width: 300,
+            height: 300,
+            colorDark: "#7e7e7e",
+            colorLight: "rgba(0,0,0,0)",
+        });
+    }
     url.value = "";
 }
